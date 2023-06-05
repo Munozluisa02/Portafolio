1f7356f36808832b8c3c4bf9adfb73135919749f
Luisa-MP <munozluisa643@gmail.com>
2023-06-05 17:24:50 -0500

    hot fix

--- personal-portafolio/src/components/NavBar.jsx
+++ personal-portafolio/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 
-export default function NavBar() {
+export default function NavBar () {
   const [activeLink, setActiveLink] = useState('home');
   const [scrolled, setScrolled] = useState(false);
 
