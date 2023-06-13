d149971dca01a3cf93d2a46fcf89353d4e47cdf0
Luisa-MP <munozluisa643@gmail.com>
2023-06-13 14:48:26 -0500

    footer component created with styles and contact styles added either. process.env fixed

--- personal-portafolio/vite.config.js
+++ personal-portafolio/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+import replace from '@rollup/plugin-replace';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    replace({
+      'process.env': JSON.stringify({
+        REACT_APP_MAILCHIMP_URL: process.env.REACT_APP_MAILCHIMP_URL,
+        REACT_APP_MAILCHIMP_U: process.env.REACT_APP_MAILCHIMP_U,
+        REACT_APP_MAILCHIMP_ID: process.env.REACT_APP_MAILCHIMP_ID
+      })
+    })
+  ],
+  define:{
+    'process.env': process.env
+  },
 })
